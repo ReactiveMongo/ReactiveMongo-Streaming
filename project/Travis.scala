8fb6b6760b8e710a3d025e26498dd9efcf16ef89
Scala Steward <me@scala-steward.org>
2020-01-04 09:24:23 +0100

    Update akka-actor, akka-slf4j, akka-stream, ... to 2.4.20

--- project/Travis.scala
+++ project/Travis.scala
@@ -6,7 +6,7 @@ object Travis {
 
   lazy val settings = Seq(
     travisEnv in Test := { // test:travisEnv from SBT CLI
-      val (akkaLower, akkaUpper) = "2.4.10" -> "2.5.23"
+      val (akkaLower, akkaUpper) = "2.4.20" -> "2.5.23"
       val (playLower, playUpper) = "2.3.10" -> "2.6.1"
       val specs = List[(String, List[String])](
         "AKKA_VERSION" -> List(akkaLower, akkaUpper),
