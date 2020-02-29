015526f900ea8807781fbee61389b2db3f83e8d0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-29 21:49:12 +0100

    Update specs2-core, specs2-junit to 4.9.0 (#145)

--- project/Common.scala
+++ project/Common.scala
@@ -46,7 +46,7 @@ object Common extends AutoPlugin {
 
       rmDeps ++ (Seq(
         "specs2-core", "specs2-junit").map(
-        "org.specs2" %% _ % "4.8.3" % Test) ++ Seq(
+        "org.specs2" %% _ % "4.9.0" % Test) ++ Seq(
         Dependencies.slf4jSimple % Test))
     }
   ) ++ Format.settings ++ Publish.settings ++ (
